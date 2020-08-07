568a394fababa48846a87422b5c85b673e29ac70
Keve1227 <killenlillen02@gmail.com>
2020-08-07 19:49:34 +0200

    Fix issue with components directly in components

--- javascript/parser.js
+++ javascript/parser.js
@@ -6,48 +6,10 @@ function parse(node, recursionChain) {
         recursionChain = new Set();
     }
 
+    let recursionChainBefore = new Set(recursionChain);
     if (node.tagName.toLowerCase() == "component") {
-        if (!node.hasAttribute("ref")) {
-            throw new Error(`Component tag missing ref attribute: ${node.outerHTML}`);
-        }
-
-        let componentName = node.getAttribute("ref").toLowerCase();
-        let component = components[componentName];
-
-        if (!component) {
-            throw new Error(`Component ${componentName} does not exist!`);
-        }
-
-        // Check for recursion
-        if (recursionChain.has(componentName)) {
-            let recursionChainStr = [...Array.from(recursionChain.keys()), componentName].map(e => {
-                if (typeof e == "symbol") {
-                    return e.description;
-                } else {
-                    return "comp:" + e + (e == componentName ? " <--" : "");
-                }
-            }).join("\n\t");
-
-            throw new Error(`Recursion detected! Stack:\n\t${recursionChainStr}`);
-        } else {
-            recursionChain.add(componentName);
-        }
-
-        let groupedAttributes = groupAttributes(node);
-
-        let newHTML = component.html;
-        newHTML = newHTML.split("$childs").join(node.innerHTML);
-        for (let argName in groupedAttributes.arguments) {
-            newHTML = newHTML.split(`{{${argName}}}`).join(groupedAttributes.arguments[argName]);
-        }
-        node = new JSDOM(newHTML).window.document.body.firstChild;
-
-        // Apply non-arg attributes
-        for (let argName in groupedAttributes.standard) {
-            node.setAttribute(argName, groupedAttributes.standard[argName]);
-        }
+        node = dereferenceComponent(node, recursionChain);
     } else {
-        recursionChain.add(Symbol(node.tagName.toLowerCase()));
         node = node.cloneNode(true);
     }
 
@@ -55,7 +17,57 @@ function parse(node, recursionChain) {
         child.replaceWith(parse(child, recursionChain));
     });
 
-    recursionChain.delete(Array.from(recursionChain.keys())[recursionChain.size - 1]);
+    recursionChain.clear();
+    recursionChainBefore.forEach(e => recursionChain.add(e));
+    return node;
+}
+
+function dereferenceComponent(node, recursionChain) {
+    if (!node.hasAttribute("ref")) {
+        throw new Error(`Component tag missing ref attribute: ${node.outerHTML}`);
+    }
+
+    let componentName = node.getAttribute("ref").toLowerCase();
+    let component = components[componentName];
+
+    if (!component) {
+        throw new Error(`Component ${componentName} does not exist!`);
+    }
+
+    // Check for recursion
+    if (recursionChain.has(componentName)) {
+        let recursionChainStr = [...Array.from(recursionChain.keys()), componentName].map(e => {
+            if (typeof e == "symbol") {
+                return e.description;
+            } else {
+                return "comp:" + e + (e == componentName ? " <--" : "");
+            }
+        }).join("\n\t");
+
+        throw new Error(`Recursion detected! Stack:\n\t${recursionChainStr}`);
+    } else {
+        recursionChain.add(componentName);
+    }
+
+    let groupedAttributes = groupAttributes(node);
+
+    let newHTML = component.html;
+    newHTML = newHTML.split("$childs").join(node.innerHTML);
+    for (let argName in groupedAttributes.arguments) {
+        newHTML = newHTML.split(`{{${argName}}}`).join(groupedAttributes.arguments[argName]);
+    }
+    node = new JSDOM(newHTML).window.document.body.firstChild;
+
+    // Apply non-arg attributes
+    for (let argName in groupedAttributes.standard) {
+        node.setAttribute(argName, groupedAttributes.standard[argName]);
+    }
+
+    // If the resulting element is a component in itself, parse it as well
+    if (node.tagName.toLowerCase() == "component") {
+        node = dereferenceComponent(node, recursionChain);
+    }
+
     return node;
 }
 
